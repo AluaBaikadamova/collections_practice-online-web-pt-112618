d447dcd56edb985f67681f07bc8e09396ededf28
Alua Baikadamova <abaikad00@gmail.com>
2019-01-04 12:35:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+end
+end
 
+def sort_array_desc(array)
+   array.sort do |a, b|
+  -(a <=> b)
+end 
+end
+
+def sort_array_char_count (array)
+   array.sort do |a, b|
+  a.length <=> b.length
+end 
+end
+
+def swap_elements (array)
+   if (array.length >1) 
+     a=array[1]
+     array[1]=array[2]
+     array[2]=a
+end 
+return array
+end
+
+def swap_elements_from_to (array,index,destination_index)
+ a=array[index]
+     array[index]=array[destination_index]
+     array[destination_index]=a
+     return array
+ end
+ 
