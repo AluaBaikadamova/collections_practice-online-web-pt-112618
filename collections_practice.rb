4dfc176094ae757e64cf99cd3e0a9ce3b79cd15e
Alua Baikadamova <abaikad00@gmail.com>
2019-01-04 13:39:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -57,7 +57,7 @@ return b
  end
  
   def sum_array (array)
-array.inject{|memo, num| memo += num if num.is_a?=='int'; memo}
+array.inject{|memo, num| memo += num if num.is_a?(Int); memo}
  end
  
   def add_s (array)
